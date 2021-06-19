528c3cc4af4fab662eab51c0bd837f45225b272f
kt-postnikova <kt.postnikova@yandex.ru>
2021-06-19 14:39:58 +0300

    deleted noValidate and added props button text

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -7,10 +7,10 @@ function PopupWithForm(props) {
             <div className={`popup popup_type_${props.name} ${props.isOpen ? ('popup_opened') : ''}`}>
                 <div className="popup__container">
                     <button className="popup__close" onClick={props.onClose} type="button" aria-label="Закрыть"></button>
-                    <form className="form" id={props.name} name={props.name} noValidate>
+                    <form className="form" id={props.name} name={props.name}>
                         <h2 className="form__header">{props.title}</h2>
                         {props.children}
-                        <button className="form__button" type="submit">Сохранить</button>
+                        <button className="form__button" type="submit">{props.button}</button>
                     </form>
                 </div>
             </div>
