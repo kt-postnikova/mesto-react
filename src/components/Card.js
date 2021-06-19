bf983bec361cf3dbb886f9bd39c762aaf0eb7e94
kt-postnikova <kt.postnikova@yandex.ru>
2021-06-19 14:40:58 +0300

    replaced code according to task

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,17 +7,19 @@ function Card(props) {
     }
 
     return (
-        <article className="element">
-            <img className="element__image" onClick={handleCardClick} alt="Картинка пользователя" src={props.card.link} />
-            <button className="element__trash-btn" type="button" aria-label="Удалить"></button>
-            <div className="element__block">
-                <h2 className="element__title">{props.card.name}</h2>
-                <div className="like">
-                    <button className="like__button" type="button" aria-label="Нравится"></button>
-                    <span className="like__counter">{props.card.likes.length}</span>
+        <>
+            <article className="element">
+                <img className="element__image" onClick={handleCardClick} alt={props.card.name} src={props.card.link} />
+                <button className="element__trash-btn" type="button" aria-label="Удалить"></button>
+                <div className="element__block">
+                    <h2 className="element__title">{props.card.name}</h2>
+                    <div className="like">
+                        <button className="like__button" type="button" aria-label="Нравится"></button>
+                        <span className="like__counter">{props.card.likes.length}</span>
+                    </div>
                 </div>
-            </div>
-        </article>
+            </article>
+        </>
     )
 }
 
